f9c949b1b5d41d599c2fdfe9dadd71ecb3cc041c
Saurav Singh <97401461+saurav-22@users.noreply.github.com>
2025-08-24 13:19:20 +0530

    Create launch-spotfleet.bat

---
+++ launch-spotfleet.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM ---- Config ----
+set JSON_FILE=spot_instance_request.json
+set KEY_FILE=C:\Users\YourUser\Downloads\saurav.pem
+set USER=ec2-user
+
+REM ---- Step 1: Request Spot Fleet ----
+for /f %%i in ('aws ec2 request-spot-fleet --spot-fleet-request-config file://%JSON_FILE% --query "SpotFleetRequestId" --output text') do set FLEET_ID=%%i
+echo Spot Fleet Request ID: %FLEET_ID%
+
+REM ---- Step 2: Wait until instance is running ----
+echo Waiting for instance to launch...
+:waitloop
+for /f %%j in ('aws ec2 describe-spot-fleet-instances --spot-fleet-request-id %FLEET_ID% --query "ActiveInstances[0].InstanceId" --output text') do set INSTANCE_ID=%%j
+if "%INSTANCE_ID%"=="None" (
+    timeout /t 10 >nul
+    goto waitloop
+)
+echo Instance launched: %INSTANCE_ID%
+
+REM ---- Step 3: Get Public IP ----
+for /f %%k in ('aws ec2 describe-instances --instance-ids %INSTANCE_ID% --query "Reservations[0].Instances[0].PublicIpAddress" --output text') do set PUBLIC_IP=%%k
+echo Public IP: %PUBLIC_IP%
+
+REM ---- Step 4: SSH into instance ----
+ssh -i "%KEY_FILE%" %USER%@%PUBLIC_IP%
